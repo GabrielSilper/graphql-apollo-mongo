274ff50c013ae25f959f213524f86e897349d379
GabrielSilper <gabrielsilper.main@gmail.com>
2023-12-10 00:26:21 -0400

    feat: models do mongo definidos, User e Post

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const { Schema, default: mongoose } = require('mongoose');
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
